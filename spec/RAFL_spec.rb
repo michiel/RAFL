0bb25428462b7a9bfe91e451b0f4c37d18e9b940
Michiel Kalkman <michiel@etryx.com>
2011-07-07 22:49:11 +0200

    Test for truth in rspec (also up'd rspec version to 2.6.0)

--- spec/RAFL_spec.rb
+++ spec/RAFL_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Rafl" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+describe "RAFL" do
+  it "should just be fine with this empty stub for now" do
+    1.should == 1
   end
 end
